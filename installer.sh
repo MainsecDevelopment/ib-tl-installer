152d4eb47ba5b40a4db66ce364ceca0e68ba3264
MainsecDevelopment <39780900+MainsecDevelopment@users.noreply.github.com>
2018-09-24 09:59:42 +0200

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,11 @@
+apt-get update
+mdkir ivan-biagi
+cd ivan-biagi
+git clone https://github.com/h0nus/RogueSploit.git
+git clone https://github.com/h0nus/BackToMe.git
+git clone https://github.com/h0nus/SpyNoteShell.git
+git clone https://github.com/h0nus/BurpSuitePro-2.0.06-beta-Loader-Keygen.git 
+git clone https://github.com/h0nus/SecureSafePRO-Password-Generator-Crack.git 
+git clone https://github.com/h0nus/Artisteer.4.3.0.60745-Crack.git 
+git clone https://github.com/h0nus/Simple-MacChanger.git 
+ls
